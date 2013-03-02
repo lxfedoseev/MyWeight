7dfe3dbb268ab3dfd708e201795183ba4815d4e2
Alex Fedoseev <Sasha@mac.local>
2013-03-02 08:54:39 +0400

    Export -> Import

--- gen/com/example/alexfed/myweight/R.java
+++ gen/com/example/alexfed/myweight/R.java
@@ -17,8 +17,8 @@ public final class R {
         public static final int layout=0x7f070000;
         public static final int menu_add=0x7f070004;
         public static final int menu_clear_db=0x7f070005;
-        public static final int menu_export_csv=0x7f070002;
-        public static final int menu_export_db=0x7f070003;
+        public static final int menu_import_csv=0x7f070002;
+        public static final int menu_import_db=0x7f070003;
         public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
@@ -35,8 +35,8 @@ public final class R {
         public static final int hello_world=0x7f040001;
         public static final int menu_add=0x7f040006;
         public static final int menu_clear_db=0x7f040007;
-        public static final int menu_export_csv=0x7f040004;
-        public static final int menu_export_db=0x7f040005;
+        public static final int menu_import_csv=0x7f040004;
+        public static final int menu_import_db=0x7f040005;
         public static final int menu_settings=0x7f040002;
     }
     public static final class style {
